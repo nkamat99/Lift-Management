265a3d359b48eb544ea7cf0585759c84b4170844
Nidhi Radhakrishna Kamat <nrkamat@oneshield.com>
2023-08-02 23:16:31 +0530

    fan

---
+++ Lift/script.js
@@ -0,0 +1,28 @@
+
+    var music = new Audio('./assets/fan.mp3');
+    let isPlaying = false;
+    function playMusic(){
+    if (isPlaying) {
+        music.pause();
+      } else {
+        music.play();
+      }
+      isPlaying = !isPlaying;
+    }
+    
+music.addEventListener('ended', function () {
+    isPlaying = false;
+  });
+  
+  music.addEventListener('play', function () {
+    isPlaying = true;
+  });
+  
+  music.addEventListener('pause', function () {
+    isPlaying = false;
+  });
+
+  
+  
+  
+  
