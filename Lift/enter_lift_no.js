19292bb6b0aa7c6c93601cc007141e194ce6aa33
Druvi Tendulkar <dtendulkar@oneshield.com>
2023-08-04 00:16:43 +0530

    Calling the lift feature

---
+++ Lift/enter_lift_no.js
@@ -0,0 +1,84 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("formid");
+
+    p.textContent = "0";
+    let liftFloor = parseInt(p.textContent);  // where the lift is currently
+    console.log(liftFloor)
+  
+    form.addEventListener("submit", async function(event) {
+      event.preventDefault(); 
+  
+      const noOfPeople = parseInt(document.getElementById("input").value);
+      const currentFloor = parseInt(document.getElementById("floor").value);  // where the user is currently
+  
+      console.log("Number of People:", noOfPeople);
+      console.log("Current Floor:", currentFloor);
+
+
+      // Calculate the difference between liftFloor and currentFloor
+      const floorDifference = currentFloor - liftFloor;
+
+      // Define the direction of movement (up or down)
+      const direction = Math.sign(floorDifference); // -1 for down, 1 for up
+
+      const interval = setInterval(function() {
+          if (liftFloor !== currentFloor) {
+              liftFloor += direction;
+              p.textContent = liftFloor.toString();
+              console.log("Current Lift Floor:", liftFloor);
+          } else {
+              clearInterval(interval);
+          }
+      }, 2000); 
+
+      // This part should display a door open animation as it moves to the inside lift console
+      window.location.href = "floor_console.html"
+
+
+    });
+});
+export default liftFloor
+
+
+
+// delete this part Druvi
+
+    //   const targetValue = currentFloor;
+// for(let i=0; i<=7; i++)
+// {
+//             setInterval(function() {
+//                 if(liftFloor<currentFloor){
+//                     p.textContent = (liftFloor+1).toString();
+//                     console.log("a",liftFloor)
+//                     liftFloor = liftFloor + 1;
+//                 }
+//                 if(liftFloor>currentFloor){
+//                     p.textContent = (liftFloor-1).toString();
+//                     console.log("b",liftFloor)
+//                     liftFloor = liftFloor - 1;
+//                 }
+//                 if(liftFloor == currentFloor)
+//                 {
+//                     console.log("c",liftFloor)
+//                     // window.location.href = "floor_console.html";
+//                 }
+                
+//             }, 7000); 
+//             // p.textContent = liftFloor.toString();
+//             i=i+1;
+//         }
+   
+  
+
+
+//   const targetValue = 10; // Replace with your desired integer value
+// let currentValue = 0;
+
+// const interval = setInterval(function() {
+//     if (currentValue <= targetValue) {
+//         console.log(currentValue);
+//         currentValue++;
+//     } else {
+//         clearInterval(interval);
+//     }
+// }, 2000); // 2000 milliseconds = 2 seconds
---
+++ Lift/floor_console.js
@@ -0,0 +1,4 @@
+
+import liftFloor from 'enter_lift_no.js';
+
+console.log(liftFloor); // Displays "Hello from File 1!" in the console
