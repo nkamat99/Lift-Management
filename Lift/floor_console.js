99eeeb4f9d5c744e7a11f9cb174e894858873daf
Druvi Tendulkar <dtendulkar@oneshield.com>
2023-08-04 14:10:48 +0530

    lift movement

--- Lift/floor_console.js
+++ Lift/floor_console.js
@@ -1,6 +1,17 @@
-// Import the value of liftFloor from file1.js
-import { liftFloor } from './enter_lift_no';
-//  const liftFloor = require('./enter_lift_no.js');
+// // Import the value of liftFloor from file1.js
+// import { liftFloor } from './enter_lift_no';
+// //  const liftFloor = require('./enter_lift_no.js');
 
-// Now you can use liftFloor in this file
-console.log("Lift Floor in floor_console.js:", liftFloor);
+// // Now you can use liftFloor in this file
+// console.log("Lift Floor in floor_console.js:", liftFloor);
+floor=sessionStorage.getItem('floor');
+console.log(floor)
+roomno=sessionStorage.getItem('roomno');
+console.log(roomno)
+
+
+document.addEventListener("DOMContentLoaded", function() {
+current_floor.textContent = floor.toString();
+
+});
+
