75314182868190655d10beeee72e10e5227aaa8c
Druvi Tendulkar <dtendulkar@oneshield.com>
2023-08-05 15:11:52 +0530

    added enable/disabble floors, Lift movement to desired floor features

--- Lift/floor_console.js
+++ Lift/floor_console.js
@@ -1,10 +1,3 @@
-// Import the value of liftFloor from file1.js
-// import { liftFloor } from './enter_lift_no';
-// //  const liftFloor = require('./enter_lift_no.js');
-
-// // Now you can use liftFloor in this file
-// console.log("Lift Floor in floor_console.js:", liftFloor);
-
 async function fetchDataAndDisplay() {
     const floor = sessionStorage.getItem('floor');
     console.log(floor);
@@ -23,43 +16,32 @@ async function fetchDataAndDisplay() {
       const responseData = await response.json();
       console.log('Response from backend:', responseData);
 
-      
       accessFloors = responseData.AccessDetails;
 
-    //   [2, 4, 7, 1]
-    // for(let i=0; i<accessFloors.length; i++)
-    // {   
-        // let i=0
+      // document.addEventListener("DOMContentLoaded", function() {
+      // accessFloors = [2, 4, 7, 1];
         for(let j=1; j<8; j++)
         {
             if(accessFloors.includes(j))
             {
-                console.log("Display", j);
+                console.log("Enable", j);
             }
             else{
-                console.log("Disable", j);
+              console.log("Disable", j);
+
+              const elements = document.querySelectorAll(`[id="${j}"]`);
+
+              elements.forEach(element => {
+                element.classList.add('disable');
+              });
+              
             }
-            
-        // }
-        // console.log(accessFloors[i]);
-        // console.log(i);
+        }
 
-        // for(let i=1; i<8; i++)
-    
-        // if( i in accessFloors){
-        //     console.log("button will be displayed", i)
-        //     // code for displaying the button
-        // }
-        // else{
-        //     console.log("button will be disabled", i)
-        //     // code for disabling
-        // }
-    }
-      
+        
+      // });
 
-
-  
-      // Display the floor value
+      // Display the floor value (meter)
       const current_floor = document.getElementById('current_floor');
       current_floor.textContent = floor.toString();
     } catch (error) {
@@ -67,6 +49,58 @@ async function fetchDataAndDisplay() {
     }
   }
   
+
+  function targetfloor(targetfloor) {
+
+    // disable all buttons so that user cant click on any other button while lift in movement
+    for(let i=0; i<8; i++)
+    {
+      const elements = document.querySelectorAll(`[id="${i}"]`);
+      elements.forEach(element => {
+        element.classList.add('disable');
+      });
+    }
+
+    let targetFloor = targetfloor.id;
+    console.log("Target Floor:", targetFloor);
+
+    let currentFloor = sessionStorage.getItem('floor');
+    // console.log(currentFloor);
+
+    // code to take lift to desired floor
+
+    // Calculate the difference between liftFloor and currentFloor
+      const floorDifference = targetFloor - currentFloor;
+      console.log(floorDifference)
+
+      // Define the direction of movement (up or down)
+      const direction = Math.sign(floorDifference); // -1 for down, 1 for up
+      console.log(direction)
+
+      if(direction == -1 ){
+        document.getElementById("up_arrow").style.visibility='hidden';
+      }
+      else{
+        document.getElementById("down_arrow").style.visibility='hidden';
+      }
+
+      currentFloor = parseInt(currentFloor);
+      targetFloor = parseInt(targetFloor);
+
+      const interval = setInterval(function() {
+          if (currentFloor !== targetFloor) {
+            currentFloor += direction;
+            current_floor.textContent = currentFloor.toString();
+              console.log("Current Lift Floor:", currentFloor);
+          } else {
+            console.log("abc");
+              window.location.href= "exit_doors.html";
+              clearInterval(interval);
+          }
+      }, 2000);
+     
+  }
+
   // Call the fetchDataAndDisplay function to initiate the process
   fetchDataAndDisplay();
   
---
+++ Lift/js/exit_doors.js
@@ -0,0 +1,38 @@
+const animatedDiv = document.getElementById('left');
+document.addEventListener('DOMContentLoaded', () => {
+    var music = new Audio('../Lift/assets/elevator_door.mp3');
+    let isPlaying = false;
+   
+    if (isPlaying) {
+        music.pause();
+      } else {
+        music.play();
+      }
+    isPlaying = !isPlaying;
+    
+    
+music.addEventListener('ended', function () {
+    isPlaying = false;
+  });
+  
+  music.addEventListener('play', function () {
+    isPlaying = true;
+  });
+  
+  music.addEventListener('pause', function () {
+    isPlaying = false;
+  });
+
+  
+});
+animatedDiv.addEventListener('animationend', () => {
+
+    animatedDiv.style.display = 'none';
+    location.replace("login.html")
+    
+});
+const animatedDivright = document.getElementById('right');
+
+animatedDivright.addEventListener('animationend', () => {
+    animatedDivright.style.display = 'none';
+});
