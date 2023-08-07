4728d9fc60a6ec957b2d783be899063d1661f229
Nidhi Radhakrishna Kamat <nrkamat@oneshield.com>
2023-08-07 15:10:57 +0530

    exit door

--- Lift/js/exit_doors.js
+++ Lift/js/exit_doors.js
@@ -1,5 +1,31 @@
 const animatedDiv = document.getElementById('left');
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async function() {
+  var floor=sessionStorage.getItem('floor');
+  console.log("floor:",floor);
+  if(floor >=1 && floor<=5 ){
+    console.log("floor:",floor);
+    // Change the background color of the body after 3 seconds:done
+
+  document.body.style.backgroundImage = "url('../Lift/assets/Room_background.jpeg')"; // Change to your desired color
+
+
+  }
+  if(floor ==6 ){
+    console.log("floor 6:",floor);
+    // Change the background color of the body after 3 seconds
+
+  document.body.style.backgroundImage = "url('../Lift/assets/gym_background.jpg')"; // Change to your desired color
+
+
+  }
+  if(floor ==7 ){
+    console.log("floor 7:",floor);
+    // Change the background color of the body after 3 seconds
+
+  document.body.style.backgroundImage = "url('../Lift/assets/Rooftop_background.jpg')"; // Change to your desired color
+
+
+  }
     var music = new Audio('../Lift/assets/elevator_door.mp3');
     let isPlaying = false;
    
@@ -28,7 +54,8 @@ music.addEventListener('ended', function () {
 animatedDiv.addEventListener('animationend', () => {
 
     animatedDiv.style.display = 'none';
-    location.replace("login.html")
+    // location.replace("login.html")
+    setTimeout(()=>{location.replace("login.html")},3000);
     
 });
 const animatedDivright = document.getElementById('right');
